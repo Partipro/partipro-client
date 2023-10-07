d78c786467d3a434cb7f837c168b3c7006be7cab
gabrieltakaki <gabitj@icloud.com>
2023-10-06 22:09:59 -0300

    feat: MUI styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import { StyledEngineProvider } from "@mui/material/styles";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import router from "./routes.tsx";
 
 const queryClient = new QueryClient();
@@ -9,7 +11,9 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
+      <StyledEngineProvider injectFirst>
+        <RouterProvider router={router} />
+      </StyledEngineProvider>
     </QueryClientProvider>
   </React.StrictMode>,
 );
