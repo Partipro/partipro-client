ab9f9bd8c0e73751b6e8ad7b7af93b293efd10a7
gabrieltakaki <gabitj@icloud.com>
2023-10-06 21:44:18 -0300

    refactor: error message and name

--- src/helpers/api-error.ts
+++ src/helpers/api-error.ts
@@ -4,7 +4,12 @@ export default class ApiError extends Error {
   constructor(error: unknown) {
     super();
 
-    this.name = get(error, "response.data.name", "unexpected_error");
+    this.name = get(error, "response.data.error.name", "unexpected_error");
+    this.message = get(
+      error,
+      "response.data.error.message",
+      "unexpected_error",
+    );
 
     if (typeof Error.captureStackTrace === "function") {
       Error.captureStackTrace(this, this.constructor);
