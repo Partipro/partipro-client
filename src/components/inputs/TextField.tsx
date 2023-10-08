eaecedb279ad5f9a3af547ff3290bde552d71c61
gabrieltakaki <gabitj@icloud.com>
2023-10-08 00:09:45 -0300

    feat: textField component

---
+++ src/components/inputs/TextField.tsx
@@ -0,0 +1,20 @@
+import MuiTextField from "@mui/material/TextField";
+import { ChangeEvent } from "react";
+
+type Props = {
+  type?: "password" | "email" | "text";
+  label?: string;
+  name: string;
+  fullWidth?: boolean;
+  value: any;
+  onChange?: (event: ChangeEvent<HTMLInputElement>) => void;
+  error?: boolean;
+  id?: string;
+  helperText?: string;
+};
+
+function TextField({ type = "text", ...props }: Props) {
+  return <MuiTextField type={type} {...props} />;
+}
+
+export default TextField;
