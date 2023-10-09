04fa7a0ccb7ae7fa4720d0413ad491691c92ba2b
gabrieltakaki <gabitj@icloud.com>
2023-10-08 22:44:44 -0300

    feat: MUI type colors

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,31 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { StyledEngineProvider } from "@mui/material/styles";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createTheme, ThemeProvider } from "@mui/material";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import router from "./routes.tsx";
 
 const queryClient = new QueryClient();
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#0a5694",
+    },
+    secondary: {
+      main: "#E33E7F",
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <StyledEngineProvider injectFirst>
-        <RouterProvider router={router} />
+        <ThemeProvider theme={theme}>
+          <RouterProvider router={router} />
+        </ThemeProvider>
       </StyledEngineProvider>
     </QueryClientProvider>
   </React.StrictMode>,
