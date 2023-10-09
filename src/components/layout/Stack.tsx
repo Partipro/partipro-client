6d4a9e50adcd159a358756e81a6b8fb1781cbaa1
gabrieltakaki <gabitj@icloud.com>
2023-10-08 22:44:04 -0300

    fix: margin in the Stack component

--- src/components/layout/Stack.tsx
+++ src/components/layout/Stack.tsx
@@ -1,13 +1,21 @@
 import React from "react";
+import Box from "@mui/material/Box";
 import MuiStack from "@mui/material/Stack";
 
 type Props = {
   spacing?: number;
   children?: React.ReactNode;
+  margin?: number;
 };
 
-function Stack({ spacing = 2, children }: Props) {
-  return <MuiStack spacing={spacing}>{children}</MuiStack>;
+function Stack({ spacing = 2, margin, children }: Props) {
+  return (
+    <Box sx={{ width: "100%" }}>
+      <MuiStack margin={margin} spacing={spacing}>
+        {children}
+      </MuiStack>
+    </Box>
+  );
 }
 
 export default Stack;
