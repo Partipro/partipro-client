28e164b6ddca8bff797c3095853382046938c92b
gabrieltakaki <gabitj@icloud.com>
2023-10-09 22:43:43 -0300

    feat: authcontext call

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { StyledEngineProvider } from "@mui/material/styles";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App.tsx";
+import AuthContext from "./context/AuthContext.tsx";
 
 const queryClient = new QueryClient();
 
@@ -10,7 +11,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <StyledEngineProvider injectFirst>
-        <App />
+        <AuthContext>
+          <App />
+        </AuthContext>
       </StyledEngineProvider>
     </QueryClientProvider>
   </React.StrictMode>,
