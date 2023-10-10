1c2ae4c7ac1a8e336758e6cfc988c2849981394c
gabrieltakaki <gabitj@icloud.com>
2023-10-09 22:44:26 -0300

    feat: useQuery methods

--- src/hooks/useQuery.tsx
+++ src/hooks/useQuery.tsx
@@ -3,16 +3,28 @@ import { useQuery as reactUseQuery } from "@tanstack/react-query";
 function useQuery<R, F>({
   queryKey,
   service,
+  autoStart = true,
 }: {
   queryKey?: [keyof F, F[keyof F]];
   service: (filters?: F[keyof F]) => Promise<R>;
-}): [boolean, R | undefined, boolean, boolean, any] {
-  const { isLoading, data, isSuccess, isError, error } = reactUseQuery({
-    queryKey: [queryKey],
-    queryFn: () => service(queryKey?.[1]),
-  });
+  autoStart?: boolean;
+}): {
+  isLoading: boolean;
+  data: R | undefined;
+  isSuccess: boolean;
+  isError: boolean;
+  error: any;
+  refetch: () => void;
+} {
+  const { isLoading, data, refetch, isSuccess, isError, error } = reactUseQuery(
+    {
+      queryKey: [queryKey],
+      queryFn: () => service(queryKey?.[1]),
+      enabled: autoStart,
+    },
+  );
 
-  return [isLoading, data, isSuccess, isError, error];
+  return { isLoading, data, isSuccess, isError, error, refetch };
 }
 
 export default useQuery;
