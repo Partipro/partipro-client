fba19a4861288303c6ce9612f17e773b652fc598
gabrieltakaki <gabitj@icloud.com>
2023-10-09 22:42:00 -0300

    feat: initial commit Contracts.tsx

---
+++ src/screens/Contracts/screens/Contracts.tsx
@@ -0,0 +1,11 @@
+import FlexColumn from "../../../components/layout/FlexColumn.tsx";
+
+function Contracts() {
+  return (
+    <FlexColumn>
+      <div>Contracts</div>
+    </FlexColumn>
+  );
+}
+
+export default Contracts;
