8b423a1728ec8f6cbc76d2b805123eb41780baf8
gabrieltakaki <gabitj@icloud.com>
2023-10-14 15:50:09 -0300

    feat: lazy import PropertiesForm.tsx

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,17 +1,20 @@
 import { lazy } from "react";
 import { createBrowserRouter, Navigate } from "react-router-dom";
-import MenuContent from "./components/others/MenuContent.tsx";
-import PropertiesForm from "./screens/Properties/screens/PropertiesForm.tsx";
 
+const Login = lazy(() => import("./screens/Authentication/screens/Login.tsx"));
+const Register = lazy(
+  () => import("./screens/Authentication/screens/Register.tsx"),
+);
+
+import MenuContent from "./components/others/MenuContent.tsx";
 const Contracts = lazy(
   () => import("./screens/Contracts/screens/Contracts.tsx"),
 );
 const Properties = lazy(
   () => import("./screens/Properties/screens/Properties.tsx"),
 );
-const Login = lazy(() => import("./screens/Authentication/screens/Login.tsx"));
-const Register = lazy(
-  () => import("./screens/Authentication/screens/Register.tsx"),
+const PropertiesForm = lazy(
+  () => import("./screens/Properties/screens/PropertiesForm.tsx"),
 );
 
 const authRoutes = createBrowserRouter([
