f41dbf56d1393ea900d8e727f94cc3de36d2dfd3
gabrieltakaki <gabitj@icloud.com>
2023-10-14 15:50:52 -0300

    feat: PropertiesForm.tsx initial commit

--- src/screens/Properties/screens/PropertiesForm.tsx
+++ src/screens/Properties/screens/PropertiesForm.tsx
@@ -1,5 +1,22 @@
+import { useNavigate, useParams } from "react-router-dom";
+import Dialog from "../../../components/feedback/Dialog.tsx";
+import TextField from "../../../components/inputs/TextField.tsx";
+
 function PropertiesForm() {
-  return <div></div>;
+  const params = useParams();
+  const navigate = useNavigate();
+
+  return (
+    <Dialog
+      title={params.id ? "Edit Property" : "Create Property"}
+      open
+      onClose={() => navigate(-1)}
+    >
+      <form style={{ width: "100%" }}>
+        <TextField name="Name" label="Name" value={123} />
+      </form>
+    </Dialog>
+  );
 }
 
 export default PropertiesForm;
