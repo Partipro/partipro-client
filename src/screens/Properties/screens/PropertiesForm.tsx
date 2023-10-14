32d9a134aadc7ba4d395d96afc160c8c2964ea3e
gabrieltakaki <gabitj@icloud.com>
2023-10-14 16:06:53 -0300

    feat: PropertiesForm.tsx fields setup

--- src/screens/Properties/screens/PropertiesForm.tsx
+++ src/screens/Properties/screens/PropertiesForm.tsx
@@ -1,6 +1,7 @@
 import { useNavigate, useParams } from "react-router-dom";
 import Dialog from "../../../components/feedback/Dialog.tsx";
 import TextField from "../../../components/inputs/TextField.tsx";
+import Grid from "../../../components/layout/Grid.tsx";
 
 function PropertiesForm() {
   const params = useParams();
@@ -13,7 +14,39 @@ function PropertiesForm() {
       onClose={() => navigate(-1)}
     >
       <form style={{ width: "100%" }}>
-        <TextField name="Name" label="Name" value={123} />
+        <Grid
+          spacing={4}
+          items={[
+            { item: <TextField name="name" label="Name" value={123} />, xs: 6 },
+            {
+              item: <TextField name="city" label="Cidade" value={123} />,
+              xs: 6,
+            },
+            {
+              item: <TextField name="address" label="Endereço" value={123} />,
+              md: 8,
+              xs: 12,
+            },
+            {
+              item: (
+                <TextField
+                  name="squareMeters"
+                  label="Metros quadrados"
+                  value={123}
+                />
+              ),
+              md: 2,
+              xs: 6,
+            },
+            {
+              item: (
+                <TextField name="monthRent" label="Aluguel (mês)" value={123} />
+              ),
+              md: 2,
+              xs: 6,
+            },
+          ]}
+        />
       </form>
     </Dialog>
   );
