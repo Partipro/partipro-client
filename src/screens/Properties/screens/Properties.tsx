121a47fcbfa0b058e8c960919d63e1db683f2e1c
gabrieltakaki <gabitj@icloud.com>
2023-10-14 15:50:37 -0300

    fix: Outlet in the main Properties.tsx route

--- src/screens/Properties/screens/Properties.tsx
+++ src/screens/Properties/screens/Properties.tsx
@@ -1,9 +1,11 @@
+import { Outlet } from "react-router-dom";
 import FlexColumn from "../../../components/layout/FlexColumn.tsx";
 
 function Properties() {
   return (
     <FlexColumn>
       <div>Properties</div>
+      <Outlet />
     </FlexColumn>
   );
 }
