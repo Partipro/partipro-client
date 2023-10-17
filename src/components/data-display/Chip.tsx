d42347f460d9c63814cf3fc8f8b16e1000421989
gabrieltakaki <gabitj@icloud.com>
2023-10-16 21:33:28 -0300

    feat: chip component

---
+++ src/components/data-display/Chip.tsx
@@ -0,0 +1,14 @@
+import MuiChip from "@mui/material/Chip";
+
+type Props = {
+  label: string;
+  color: "success" | "primary" | "error" | "info" | "warning";
+};
+
+function Chip(props: Props) {
+  return (
+    <MuiChip sx={{ height: "24px !important", fontSize: "12px" }} {...props} />
+  );
+}
+
+export default Chip;
