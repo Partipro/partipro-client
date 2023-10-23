339c1859dd1b6ae7a1efbf309796713d97574c1a
gabrieltakaki <gabitj@icloud.com>
2023-10-23 18:54:10 -0300

    fix: way to send the data to the backend when creating a property, as it accepts an image

--- src/screens/Properties/services/postProperty.ts
+++ src/screens/Properties/services/postProperty.ts
@@ -8,13 +8,30 @@ type Data = {
   address?: string;
   squareMeters?: number;
   monthRent?: number;
+  image?: string;
 };
 
+type Field = keyof Data;
+
 function postProperty(data: Data): Promise<Property> {
+  const formData = new FormData();
+  Object.keys(data).forEach((field: string) => {
+    if (data && data[field as Field]) {
+      formData.append(
+        field,
+        field === "image"
+          ? data.image || ""
+          : data[field as Field]?.toString() || "",
+      );
+    }
+  });
   return request({
     method: "post",
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
     url: "properties",
-    data,
+    data: formData,
   });
 }
 
