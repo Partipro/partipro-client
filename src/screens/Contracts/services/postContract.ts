4081764167a1e7cb3b7264cc7e9de69b5cd5dcfc
gabrieltakaki <gabitj@icloud.com>
2024-04-20 17:05:16 -0300

    feat: post contract service

---
+++ src/screens/Contracts/services/postContract.ts
@@ -0,0 +1,36 @@
+import request from "../../../helpers/request.ts";
+import PropertyContract from "../../../models/PropertyContract.ts";
+
+export type CreatePropertyContractProps = {
+  property: string;
+  renter: string;
+  document: string;
+};
+
+type Field = keyof CreatePropertyContractProps;
+
+function postContracts(
+  data: CreatePropertyContractProps,
+): Promise<PropertyContract> {
+  const formData = new FormData();
+  Object.keys(data).forEach((field: string) => {
+    if (data && data[field as Field]) {
+      formData.append(
+        field,
+        field === "document"
+          ? data.document || ""
+          : data[field as Field]?.toString() || "",
+      );
+    }
+  });
+  return request({
+    method: "post",
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+    url: "properties-contracts",
+    data: formData,
+  });
+}
+
+export default postContracts;
