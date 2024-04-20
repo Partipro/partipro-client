822a265b2dc243715a70da97d02d09b37a5ce2ee
gabrieltakaki <gabitj@icloud.com>
2024-04-20 17:04:49 -0300

    feat: getContracts with search params

--- src/screens/Contracts/services/getContracts.ts
+++ src/screens/Contracts/services/getContracts.ts
@@ -2,12 +2,19 @@ import request from "../../../helpers/request.ts";
 import PropertyContract from "../../../models/PropertyContract.ts";
 import { Paginated } from "../../../models/Paginated.ts";
 
+export type ContractsSearchParams = {
+  status?: string;
+  signedAt?: string;
+};
+
 export type Props = {
   page: number;
   pageSize?: number;
 };
 
-function getContracts(params: Props): Promise<Paginated<PropertyContract>> {
+function getContracts(
+  params: Props & ContractsSearchParams,
+): Promise<Paginated<PropertyContract>> {
   return request({
     url: "properties-contracts",
     params,
